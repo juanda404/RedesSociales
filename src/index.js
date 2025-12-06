b3656e78ec7208a7dc13fff6ac85ff38726ec935
Juan David Santamaría <juanda404@gmail.com>
2025-12-05 23:19:05 -0500

    Termiando ajustes

---
+++ src/index.js
@@ -0,0 +1,140 @@
+const $links = document.getElementById("links");
+const $name = document.querySelector("h1");
+const $nickname = document.querySelector("h2");
+const $description = document.getElementById("description");
+const $avatar = document.getElementById("avatar");
+const $biografia = document.getElementById("bio");
+const $sociales = document.getElementById("socialMedia");
+const $footer = document.getElementById("footer");
+
+const data = {
+  name: "Juan David Santamaría Gómez 🇨🇴",
+  nickname: "@juanda404",
+  description:
+    "Front-End Developer, built my first website in 2001. Currently a Freelance Developer. #JavaScript #React 🇨🇴",
+  avatar:
+    "https://avatars.githubusercontent.com/u/118393998?v=4",
+  bio: "Frontend developer constantly learning React.js. Currently working as a freelancer. Gym enthusiast and always striving to improve.",
+  social: [
+    {
+      name: "youtube",
+      url: "https://youtube.com/@juandavidsantamariagomez?si=TGay7o8V99As9NHA",
+      path: "M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z",
+      username: "@juandavidsantamariagomez?si=TGay7o8V99As9NHA",
+      color: "[#FF0000] ",
+    },
+    {
+      name: "github",
+      url: "https://github.com/juanda404",
+      path: "M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z",
+      username: "juanda404",
+      color: "[#6e5494]  ",
+    },
+    {
+      name: "instagram",
+      url: "https://www.instagram.com/juanda404/",
+      path: "M12 0C8.74 0 8.333.015 7.053.072 5.775.132 4.905.333 4.14.63c-.789.306-1.459.717-2.126 1.384S.935 3.35.63 4.14C.333 4.905.131 5.775.072 7.053.012 8.333 0 8.74 0 12s.015 3.667.072 4.947c.06 1.277.261 2.148.558 2.913.306.788.717 1.459 1.384 2.126.667.666 1.336 1.079 2.126 1.384.766.296 1.636.499 2.913.558C8.333 23.988 8.74 24 12 24s3.667-.015 4.947-.072c1.277-.06 2.148-.262 2.913-.558.788-.306 1.459-.718 2.126-1.384.666-.667 1.079-1.335 1.384-2.126.296-.765.499-1.636.558-2.913.06-1.28.072-1.687.072-4.947s-.015-3.667-.072-4.947c-.06-1.277-.262-2.149-.558-2.913-.306-.789-.718-1.459-1.384-2.126C21.319 1.347 20.651.935 19.86.63c-.765-.297-1.636-.499-2.913-.558C15.667.012 15.26 0 12 0zm0 2.16c3.203 0 3.585.016 4.85.071 1.17.055 1.805.249 2.227.415.562.217.96.477 1.382.896.419.42.679.819.896 1.381.164.422.36 1.057.413 2.227.057 1.266.07 1.646.07 4.85s-.015 3.585-.074 4.85c-.061 1.17-.256 1.805-.421 2.227-.224.562-.479.96-.899 1.382-.419.419-.824.679-1.38.896-.42.164-1.065.36-2.235.413-1.274.057-1.649.07-4.859.07-3.211 0-3.586-.015-4.859-.074-1.171-.061-1.816-.256-2.236-.421-.569-.224-.96-.479-1.379-.899-.421-.419-.69-.824-.9-1.38-.165-.42-.359-1.065-.42-2.235-.045-1.26-.061-1.649-.061-4.844 0-3.196.016-3.586.061-4.861.061-1.17.255-1.814.42-2.234.21-.57.479-.96.9-1.381.419-.419.81-.689 1.379-.898.42-.166 1.051-.361 2.221-.421 1.275-.045 1.65-.06 4.859-.06l.045.03zm0 3.678c-3.405 0-6.162 2.76-6.162 6.162 0 3.405 2.76 6.162 6.162 6.162 3.405 0 6.162-2.76 6.162-6.162 0-3.405-2.76-6.162-6.162-6.162zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm7.846-10.405c0 .795-.646 1.44-1.44 1.44-.795 0-1.44-.646-1.44-1.44 0-.794.646-1.439 1.44-1.439.793-.001 1.44.645 1.44 1.439z",
+      username: "juanda404",
+      color: "[#E4405F]",
+    },
+    {
+      name: "twitter",
+      url: "https://twitter.com/juanda4042",
+      path: "M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z",
+      username: "juanda4042",
+      color: "[#00A6ED]",
+    },
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/juan-david-santamaria/",
+      path: "M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.454C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.225 0z",
+      username: "juan-david-santamaria/",
+      color: "[#0077B5]",
+    },
+  ],
+  links: [
+    {
+      name: "Platzi",
+      url: "https://platzi.com/r/juanda404",
+      color: "#122f54",
+      emoji: "💻",
+    },
+    {
+      name: "Entrenamiento",
+      url: "https://librarybooksp.netlify.app/",
+      color: "#17599e",
+      emoji: "💪 ",
+    },
+    {
+      name: "blog",
+      url: "https://mylifeincodeandtraining.blogspot.com/",
+      color: "#1da6c4",
+      emoji: "💬",
+    },
+    {
+      name: "motivation",
+      url: "https://stoicismo.netlify.app/",
+      color: "#20dbd8",
+      emoji: "✍",
+    },
+    {
+      name: "projects",
+      url: "https://jdsantamaria.netlify.app/",
+      color: "#b4edeb",
+      emoji: "🚧",
+    },
+  ],
+  footer: "📩 For inquiries: juandavidsantamariag@gmail.com",
+};
+
+const main = () => {
+  let name = document.createTextNode(data?.name);
+  let nickname = document.createTextNode(data?.nickname);
+  let description = document.createTextNode(data?.description);
+  let biografia = document.createTextNode(data?.bio);
+  let avatar = document.createElement("img");
+  avatar.src = data?.avatar;
+  avatar.className = "rounded-lg w-48";
+  avatar.alt = "Juan David Santamaría";
+
+  let links = data?.links
+    ?.map((link) => {
+      return `  <div class="bg-[${link.color}] px-4 py-5 w-full flex justify-between">
+          <a class="text-sm font-bold text-white text-center hover:text-black cursor-pointer"
+            href="${link.url}" target="_blank">
+            ${link.name}
+          </a>
+          <span>${link.emoji}</span>
+        </div>`;
+    })
+    .join("");
+
+  let sociales = data?.social
+    ?.map((social) => {
+      return `<a href="${social.url}" target="_blank">
+                <svg class="w-6 h-6 cursor-pointer text-gray-400 hover:text-${social.color} fill-current" viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <title>${social.name}</title>
+                  <path
+                    d="${social.path}" />
+                </svg>
+              </a>`;
+    })
+    .join("");
+
+  let footers = document.createTextNode(data?.footer);
+
+  $sociales.innerHTML = sociales;
+
+  let newItem = document.createElement("section");
+  newItem.innerHTML = links;
+  $links.appendChild(newItem);
+  $name.appendChild(name);
+  $nickname.appendChild(nickname);
+  $description.appendChild(description);
+  $avatar.appendChild(avatar);
+  $biografia.appendChild(biografia);
+  $footer.appendChild(footers);
+};
+
+main();
